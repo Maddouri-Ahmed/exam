aafa41c1839e732e6b5189e3fed574a881f58d3a
Maddouri-Ahmed <145608692+Maddouri-Ahmed@users.noreply.github.com>
2024-01-10 01:26:13 +0100

    1.0.0

---
+++ src/main/java/com/example/examenahmed/ExamenAhmedApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenahmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAhmedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenAhmedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examenahmed/ExamenAhmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenahmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAhmedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
